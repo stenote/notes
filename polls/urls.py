0277412ae2e00b7d421ca7a4ead37482532d6564
Rui Ma <xjmarui@gmail.com>
2016-10-12 21:37:33 +0800

    1. url 中可设置 app_name 来限定 template 中的 url 前缀 2. render 可直接选择 template 进行渲染 3. get_object_or_404 get_list_or_404 都可直接进行 object 获取, 只不过 object 用 get. list 用 filter 4. template 中的 url 很方便, 可避免书写 "写死" 的 url 地址

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+from .apps import PollsConfig
+
+app_name = PollsConfig.name
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
