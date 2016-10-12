fab211bdf0906e9b0d711b80abcc0bc010d3a6e3
Rui Ma <xjmarui@gmail.com>
2016-10-12 13:01:29 +0800

    修正 tz 的错误 tz 可以查看 /usr/share/zoneinfo/
    增加 admin.site.register(Question), admin.site.register(Choice)

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world, You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at resultes of %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
