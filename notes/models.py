cf3cd0e74878368c52ae157898fdfa7cde8c5fb5
Rui Ma <xjmarui@gmail.com>
2016-10-31 22:09:04 +0800

    删掉标题, 自动创建标题 增加时间

--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    time = models.DateField(auto_now=True)
 
     def __str__(self):
-
         return self.title
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
 from django.http import HttpResponseRedirect
-
 from django.urls import reverse
 
 
@@ -11,8 +9,9 @@ from .models import Note
 
 
 def list(request):
+
     return render(request, 'list.html', {
-        'notes': Note.objects,
+        'notes': Note.objects.order_by('-id'),
     })
 
 
@@ -20,10 +19,10 @@ def add(request):
 
     if request.method == 'POST':
 
-        title = request.POST['title']
         text = request.POST['text']
-
+        title = text.split('\n')[0].strip('# ')
         note = Note(title=title, text=text)
+
         note.save()
 
         return HttpResponseRedirect(reverse('notes:view', args=(note.id, )))
@@ -34,6 +33,7 @@ def add(request):
 def view(request, note_id):
 
     note = get_object_or_404(Note, pk=note_id)
+
     return render(request, 'view.html', {
         'note': note
     })
