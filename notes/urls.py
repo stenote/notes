bb2c9e48b8d8f361060ba4ea29509c66672c7a53
Rui Ma <xjmarui@gmail.com>
2016-11-10 21:53:34 +0800

    开发完成:
    * 修改功能
    * 密码功能
    * 删除功能
    * 预览页面增加 修改，删除功能

--- notes/urls.py
+++ notes/urls.py
@@ -7,6 +7,7 @@ app_name = 'notes'
 urlpatterns = [
     url(r'^$', views.list, name='list'),
     url(r'^add$', views.add, name='add'),
+    url(r'^edit/(?P<note_ref>[0-9a-z-]+)/$', views.edit, name='edit'),
     url(r'^delete/(?P<note_ref>[0-9a-z-]+)/$', views.delete, name='delete'),
     url(r'^view/(?P<note_ref>[0-9a-z-]+)/$', views.view, name='view')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
 
 from .models import Note
 
 # Create your views here.
-
-
 def list(request):
 
     return render(request, 'list.html', {
@@ -21,13 +19,41 @@ def add(request):
 
         text = request.POST['text']
         title = text.split('\n')[0].strip('# ')
-        note = Note(title=title, text=text)
+        passwd = request.POST['passwd']
+        note = Note(title=title, text=text, passwd=passwd)
+
+        if len(passwd):
+            request.session[note_ref] = True
 
         note.save()
 
         return HttpResponseRedirect(reverse('notes:view', args=(note.ref, )))
     else:
         return render(request, 'add.html')
+
+def edit(request, note_ref):
+
+    note = get_object_or_404(Note, ref=note_ref)
+
+    if request.method == 'POST':
+        text = request.POST['text']
+        title = text.split('\n')[0].strip('# ')
+        passwd = request.POST['passwd']
+
+        note.text = text
+        note.title = title
+        note.passwd = passwd
+
+        if len(passwd):
+            request.session[note_ref] = True
+
+        note.save()
+
+        return HttpResponseRedirect(reverse('notes:view', args=(note.ref, )))
+
+    return render(request, 'edit.html', {
+        'note': note
+    })
 
 
 def delete(request, note_ref):
@@ -43,6 +69,17 @@ def view(request, note_ref):
 
     note = get_object_or_404(Note, ref=note_ref)
 
-    return render(request, 'view.html', {
-        'note': note
-    })
+    note_passwd_len = len(note.passwd)
+
+    if request.method == 'POST' and note.passwd == request.POST['passwd']:
+
+        request.session[note_ref] = True
+
+
+    if note_passwd_len == 0 or note_ref in request.session:
+
+        return render(request, 'view.html', {
+            'note': note
+        })
+
+    return HttpResponse(render(request, 'passwd.html'), status=401)
