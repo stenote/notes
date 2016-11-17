03d9867427bedff4f419342113b5db4874f992cd
Rui Ma <xjmarui@gmail.com>
2016-11-17 21:20:58 +0800

    完全清除 polls

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
